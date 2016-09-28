432a828cb032561f1b60195c497b8eee84897409
Dave Parfitt <dparfitt@chef.io>
2016-09-28 07:36:58 -0400

    popen2e Signed-off-by: Dave Parfitt <dparfitt@chef.io>

--- test/spec/platform.rb
+++ test/spec/platform.rb
@@ -356,14 +356,14 @@ module HabTesting
             ENV["RUST_LOG"] = "debug"
             output_log = open(log_file_name(), 'a')
             begin
-                Open3.popen3(ENV, fullcmdline) do |stdin, stdout, stderr, wait_thread|
+                Open3.popen2e(ENV, fullcmdline) do |stdin, stdout_err, wait_thread|
                     @all_children << [fullcmdline, wait_thread.pid]
                     puts "Started child process id #{wait_thread[:pid]}" if debug
                     found = false
                     begin
                         Timeout::timeout(timeout) do
                             loop do
-                                line = stdout.readline()
+                                line = stdout_err.readline()
                                 output_log.puts line
                                 puts line if debug
                                 if line.include?(desired_output) then
